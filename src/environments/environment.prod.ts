35451954eaac232ffb8250f32e1135887554325a
paras16jul1991 <paras16jul1991@gmail.com>
2020-04-22 01:33:37 +0530

    updated prod configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true ,
-  apiUrl : 'https://raven-ui.herokuapp.com/api'
+  apiUrl : 'https://raven-ui.herokuapp.com/api',
+  firebase: {
+    apiKey: 'AIzaSyCidkPqMo4W1xufphNIuchcBrvZiCEi9t0',
+    authDomain: 'raven-5f369.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'raven-5f369',
+    storageBucket: 'raven-5f369.appspot.com',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
